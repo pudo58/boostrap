db7e7ba4a512e774bdc7fce9f275d9878b1e2eb5
lavantho0508 <91973892+lavantho0508@users.noreply.github.com>
2022-02-25 09:43:52 +0700

    tholvph13911

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let trangchu=document.getElementById('trangchu')
+let gioithieu=document.getElementById('gioithieu')
+let lienhe=document.getElementById('lienhe')
+let gopy=document.getElementById('gopy')
+let hoidap=document.getElementById('hoidap')
+function changedVietNam(){
+trangchu.innerHTML="Trang chủ"
+gioithieu.innerHTML="Giới thiệu"
+lienhe.innerHTML="Liên hệ"
+gopy.innerHTML="Góp ý"
+hoidap.innerHTML="Hỏi đáp"
+}
+function changedEnglish(){
+    trangchu.innerHTML="Home page"
+gioithieu.innerHTML="Introduce"
+lienhe.innerHTML="Contact"
+gopy.innerHTML="Feedback"
+hoidap.innerHTML="Question"
+}
